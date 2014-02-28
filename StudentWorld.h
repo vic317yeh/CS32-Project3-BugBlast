f6aa59cc5184c388d18c8dc04bb7661f3f506e78
Vic <vic317_yeh@hotmail.com>
2014-02-28 14:53:13 -0800

    Update StudentWorld.h

--- StudentWorld.h
+++ StudentWorld.h
@@ -7,6 +7,7 @@
 #include "GameConstants.h"
 #include "Actor.h"
 #include "Level.h"
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -32,15 +33,63 @@ public:
     
     bool hasPbrick(int x, int y);
     
-    Actor* getExit();
-    
+    bool hasbrick(int x, int y);
+        
     void increaseLevel();
+    
+    void addActor(Actor* a);
+    
+    void getPlayerLocation(int& x, int& y) const;
+    
+    void applyBugSprayToActorsAt(int x, int y);
+    
+    void removeDeadObjects();
+    
+    void playerIncreaseBugSprayer();
+    
+    bool hasBugSprayerAt(int x, int y);
+    
+    bool anyLiveZumi() const;
+    
+    void setLevelFinished();
+    
+    bool levelFinished() const;
+    
+    bool zumiAndSprayersBlockedAt(int x, int y) const;
+    
+    bool hasZumiAt(int x, int y) const;
+    
+    void updateDisplayText();
+    
+    string setDisplayText(int score, int level, int livesLeft, unsigned int bonus);
+    
+    int getBonus() const;
+    
+    void decBonus();
+    
+    void activatePlayerWalkThroughWalls();
+    
+    void increasePlayerSimultaneousSprayers();
+    
+    void setPlayerDead();
+    
+    
+    // Report the value of the level data file options
+    unsigned int getProbOfGoodieOverall() const;
+    unsigned int getProbOfExtraLifeGoodie() const;
+    unsigned int getProbOfWalkThruGoodie() const;
+    unsigned int getProbOfMoreSprayersGoodie() const;
+    unsigned int getGoodieLifetimeInTicks() const;
+    unsigned int getComplexZumiSearchDistance() const;
 
+    
 private:
     Level lev;
     int curLev;
+    bool finished;
     Player* m_player;
     vector<Actor*> m_actors;
+    int bonus;
 };
 
 
